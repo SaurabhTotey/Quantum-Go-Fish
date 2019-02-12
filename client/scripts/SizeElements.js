d6d8304e9143dcfc97a403a904347ded9cb9160c
Saurabh Totey <SaurabhTotey@gmail.com>
2019-02-12 12:49:29 -0700

    Removed unnecessary front-end JavaScript code

--- client/scripts/SizeElements.js
+++
@@ -1,6 +0,0 @@
-
-let screenElement = document.getElementById("screen");
-let consoleElement = document.getElementById("console");
-
-screenElement.style.height = window.innerHeight;
-consoleElement.style.height = window.innerHeight;
