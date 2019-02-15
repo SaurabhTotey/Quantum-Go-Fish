d355df2cb8f18ebb2972a2296a25c29a8f0e6a70
Saurabh Totey <SaurabhTotey@gmail.com>
2019-02-15 08:26:24 -0700

    Made buttons look nicer and gave them clicking behaviour

--- client/scripts/CanvasManager.js
+++ client/scripts/CanvasManager.js
@@ -3,6 +3,9 @@
 let imageSize = 100;
 let padding = 5;
 
+/**
+ * A class that represents a button and defines its bounding box and how it is drawn and how it behaves
+ */
 class Button {
 
     constructor(imageSrc, x, y, w, h) {
@@ -19,9 +22,9 @@ class Button {
     draw(renderer) {
         renderer.fillStyle = "white";
         if (this.isSelected) {
-            renderer.fillStyle = "blue";
+            renderer.fillStyle = "#0074D9";
         } else if (this.isHovered) {
-            renderer.fillStyle = "lightblue";
+            renderer.fillStyle = "#7FDBFF";
         }
         renderer.fillRect(this.x, this.y, this.w, this.h);
         renderer.strokeRect(this.x, this.y, this.w, this.h);
@@ -34,7 +37,7 @@ class Button {
 
 }
 
-//Sets the canvas element up
+//Sets up the canvas element
 let canvas = document.getElementById("screen");
 canvas.setAttribute("width", `${canvas.offsetWidth}`);
 canvas.setAttribute("height", `${canvas.offsetHeight}`);
@@ -44,11 +47,32 @@ canvas.strokeStyle = "black";
 //Starts loading the images for the canvas buttons and sets their positions
 let drawModeButton = new Button("../assets/pencil.png", padding, canvas.height - padding - imageSize, imageSize, imageSize);
 let eraseModeButton = new Button("../assets/eraser.png", 2 * padding + imageSize, canvas.height - padding - imageSize, imageSize, imageSize);
+drawModeButton.isSelected = true;
 
-//Checks where the mouse is and whether it is hovering over any element
+/**
+ * Checks where the mouse is and whether it is hovering over any button
+ */
 canvas.onmousemove = (mouseEvent) => {
     drawModeButton.isHovered = drawModeButton.contains(mouseEvent.x, mouseEvent.y);
     eraseModeButton.isHovered = eraseModeButton.contains(mouseEvent.x, mouseEvent.y);
+    if (drawModeButton.isHovered || eraseModeButton.isHovered) {
+        canvas.style.cursor = "pointer";
+    } else {
+        canvas.style.cursor = "default";
+    }
+};
+
+/**
+ * Checks whether the user clicked on any button
+ */
+canvas.onclick = (mouseEvent) => {
+    if (drawModeButton.contains(mouseEvent.x, mouseEvent.y)) {
+        drawModeButton.isSelected = true;
+        eraseModeButton.isSelected = false;
+    } else if (eraseModeButton.contains(mouseEvent.x, mouseEvent.y)) {
+        drawModeButton.isSelected = false;
+        eraseModeButton.isSelected = true;
+    }
 };
 
 /**
