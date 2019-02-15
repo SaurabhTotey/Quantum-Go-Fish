8679966a5a8122942f147684fc7b8fd5ec9b4b36
Saurabh Totey <SaurabhTotey@gmail.com>
2019-02-15 08:15:39 -0700

    Made buttons highlight on hover

--- client/scripts/CanvasManager.js
+++ client/scripts/CanvasManager.js
@@ -3,25 +3,58 @@
 let imageSize = 100;
 let padding = 5;
 
-//Starts loading the images for the canvas buttons
-let drawModeButton = new Image();
-drawModeButton.src = "../assets/pencil.png";
-let eraseModeButton = new Image();
-eraseModeButton.src = "../assets/eraser.png";
+class Button {
 
-//Gets the canvas element from the DOM and gets a renderer for it
+    constructor(imageSrc, x, y, w, h) {
+        this.image = new Image();
+        this.image.src = imageSrc;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.isHovered = false;
+        this.isSelected = false;
+    }
+
+    draw(renderer) {
+        renderer.fillStyle = "white";
+        if (this.isSelected) {
+            renderer.fillStyle = "blue";
+        } else if (this.isHovered) {
+            renderer.fillStyle = "lightblue";
+        }
+        renderer.fillRect(this.x, this.y, this.w, this.h);
+        renderer.strokeRect(this.x, this.y, this.w, this.h);
+        renderer.drawImage(this.image, this.x, this.y, this.w, this.h);
+    }
+
+    contains(x, y) {
+        return x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.h;
+    }
+
+}
+
+//Sets the canvas element up
 let canvas = document.getElementById("screen");
 canvas.setAttribute("width", `${canvas.offsetWidth}`);
 canvas.setAttribute("height", `${canvas.offsetHeight}`);
 let renderer = canvas.getContext("2d");
+canvas.strokeStyle = "black";
+
+//Starts loading the images for the canvas buttons and sets their positions
+let drawModeButton = new Button("../assets/pencil.png", padding, canvas.height - padding - imageSize, imageSize, imageSize);
+let eraseModeButton = new Button("../assets/eraser.png", 2 * padding + imageSize, canvas.height - padding - imageSize, imageSize, imageSize);
+
+//Checks where the mouse is and whether it is hovering over any element
+canvas.onmousemove = (mouseEvent) => {
+    drawModeButton.isHovered = drawModeButton.contains(mouseEvent.x, mouseEvent.y);
+    eraseModeButton.isHovered = eraseModeButton.contains(mouseEvent.x, mouseEvent.y);
+};
 
 /**
  * Draws canvas buttons on the canvas and prevents drawing over the buttons
  */
 window.setInterval(() => {
-    renderer.clearRect(0, canvas.height, 3 * padding + 2 * imageSize, 2 * padding + imageSize);
-    renderer.strokeRect(padding, canvas.height - padding - imageSize, imageSize, imageSize);
-    renderer.strokeRect(2 * padding + imageSize, canvas.height - padding - imageSize, imageSize, imageSize);
-    renderer.drawImage(drawModeButton, padding, canvas.height - padding - imageSize, imageSize, imageSize);
-    renderer.drawImage(eraseModeButton, 2 * padding + imageSize, canvas.height - padding - imageSize, imageSize, imageSize);
+    drawModeButton.draw(renderer);
+    eraseModeButton.draw(renderer);
 }, 17);
