c3e4a6046e6ae753df06df19d737242b5d9350c2
Saurabh Totey <SaurabhTotey@gmail.com>
2019-02-25 15:19:42 -0700

    Made a starting message

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ io.on("connection", socket => {
      * Makes an identity for the user that just connected
      */
     let id = IdentityManager.makeId(socket);
-    IdentityManager.ids[id].log = [new Message.Message(Message.defaultSender, "INFO", "TODO: THESE WILL BE GAME INSTRUCTIONS")];
+    IdentityManager.ids[id].log = [new Message.Message(Message.defaultSender, "INFO", `Welcome to Quantum Go Fish!!! Instructions are described <a href='https://stacky.net/wiki/index.php?title=Quantum_Go_Fish' target="_blank">here</a>. Source code for this website can be found <a href='https://github.com/SaurabhTotey/Quantum-Go-Fish' target="_blank">here</a>. Your ID is ${id}. The canvas is for you to let you keep track of stuff. To start a game, type out 'TODO: IMPLEMENT COMMANDS!!'`)];
 
     /*
      * Sends the user their identity and deletes the identity on user disconnect
