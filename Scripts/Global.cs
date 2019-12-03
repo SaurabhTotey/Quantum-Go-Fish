44220f8eb8902e3f9064bc6ad1b3f2f2b9f3dbb1
Saurabh Totey <SaurabhTotey@gmail.com>
2019-12-03 15:30:59 -0700

    Added more project structure, libraries, and debug info

--- Scripts/Global.cs
+++ Scripts/Global.cs
@@ -1,5 +1,20 @@
-public class Global {
+using Godot;
+using Steamworks;
 
-	public static bool? IsHost = null;
+/**
+ * A global singleton that stores persistent data
+ */
+public class Global : Node {
+
+	//Whether the user is a host (true) or a client (false); null means the user is neither a host nor a client
+	public bool? IsHost = null;
+
+	/**
+	 * 
+	 */
+	public override void _Ready() {
+		SteamAPI.Init();
+		GD.Print("Entering game as " + SteamFriends.GetPersonaName() + ".");
+	}
 
 }
--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -1,20 +1,30 @@
 using Godot;
-using System;
-using Godot.Collections;
 
+/**
+ * Main menu
+ */
 public class MainMenu : Node {
 
+	/**
+	 * When the main menu loads up, connect up the create and join buttons to the OnMenuButtonPressed method
+	 * Also ensure that the user is set to be neither a host nor a client
+	 */
 	public override void _Ready() {
-		Global.IsHost = null;
+		GD.Print("Entering Main Menu.");
+		this.GetNode<Global>("/root/Global").IsHost = null;
 		this.GetNode("LobbyCreateButton").Connect("pressed", this, nameof(this.OnMenuButtonPressed),
 			new Godot.Collections.Array {true});
 		this.GetNode("LobbyJoinButton").Connect("pressed", this, nameof(this.OnMenuButtonPressed),
 			new Godot.Collections.Array {false});
 	}
 
+	/**
+	 * Moves the user to the lobby screen as either a host or a client
+	 */
 	public void OnMenuButtonPressed(bool isHost) {
-		Global.IsHost = isHost;
-		this.GetTree().ChangeScene("res://Lobby.tscn");
+		GD.Print("Setting user to be a " + (isHost ? "Host" : "Client") + ".");
+		this.GetNode<Global>("/root/Global").IsHost = isHost;
+		this.GetTree().ChangeScene("res://Scenes/Lobby.tscn");
 	}
 
 }
