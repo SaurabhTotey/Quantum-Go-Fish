eb04cbb0b8cd3a07a4dab483a119424bcd3c1636
SaurabhTotey <SaurabhTotey@gmail.com>
2020-05-07 21:21:04 -0600

    Fixed small error and made function definition

--- src/linuxMain/kotlin/com.saurabhtotey.quantumgofish/logic/Game.kt
+++ src/linuxMain/kotlin/com.saurabhtotey.quantumgofish/logic/Game.kt
@@ -6,5 +6,19 @@ import com.saurabhtotey.quantumgofish.network.User
  * A class that stores all the information for the game and handles/facilitates the logic
  */
 class Game(users: List<User>) {
+
+	//The players of the game
 	val players = users.map { Player(it) }
+
+	//The game's type manager
+	val typeManager = TypeManager(this.players)
+
+	/**
+	 * TODO: runs a turn of the game and returns the winner if any
+	 */
+	fun executeTurn(): Player? {
+
+		return null
+	}
+
 }
